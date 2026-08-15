3353b49de608ab965527880a638b62bb81f99b6e
Shani Sharma <117522728+27shani@users.noreply.github.com>
2026-08-15 21:00:46 +0530

    Create lenis.ts

---
+++ lib/lenis.ts
@@ -0,0 +1,11 @@
+import Lenis from '@studio-freight/lenis'
+
+export const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  orientation: 'vertical',
+  gestureOrientation: 'vertical',
+  smoothWheel: true,
+  wheelMultiplier: 1,
+  touchMultiplier: 2,
+})
